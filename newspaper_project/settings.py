93aa49b130b00bc63dc2b4062978d6a5c5ad8da7
Burak Can Kahraman <brkcnkhrmn@gmail.com>
2019-12-02 16:54:18 +0300

    Removed STATIC_ROOT from settings preventing runserver command.

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -122,7 +122,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'templates')
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'users.CustomUser'
