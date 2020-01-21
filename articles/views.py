00ab7eda68e350b092e85eac54a5d4b110035db1
Rohan Gupta <rohaninjmu@gmail.com>
2020-01-21 20:24:09 +0530

    Update views.py

--- articles/views.py
+++ articles/views.py
@@ -14,7 +14,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 
 @login_required
 def home(request):
-    return render(request, 'templates/home.html')
+    return render(request, 'templates/index.html')
 
 
 @login_required
