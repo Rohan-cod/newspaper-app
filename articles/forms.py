de1a1773edb55c7a780c0e630b4e65d0fc268c96
Rohan-cod <rohaninjmu@gmail.com>
2020-02-12 18:57:09 +0530

    Speech recognition

--- articles/forms.py
+++ articles/forms.py
@@ -1,8 +1,17 @@
 from django import forms
 from .models import Article
+from django import forms
 
 class ArticleForm(forms.ModelForm):
 
-    class Meta:
-        model = Article
-        fields = ['title', 'body', 'pic']
+	body = forms.CharField(widget=forms.TextInput(
+		attrs={
+		'id':'bod',
+
+		'placeholder' :'Write an article...',
+		}
+
+		))
+	class Meta:
+		model = Article
+		fields = ['title', 'body', 'pic']
---
+++ articles/spre.py
@@ -0,0 +1,17 @@
+import speech_recognition as sr
+
+r=sr.Recognizer()
+m=sr.Microphone()
+t=""
+
+with m as source:
+	r.adjust_for_ambient_noise(source)
+	audio = r.listen(source)
+	try:
+		t+=r.recognize_google(audio)
+	except:
+		t+=""
+
+
+print(t)
+
